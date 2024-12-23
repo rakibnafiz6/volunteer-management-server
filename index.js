b3ee2915133732e182f957ee81db99f9ae4122fb
nafiz rakib <rakibnafiz6@gmail.com>
2024-12-23 15:04:50 +0600

    added the get method with volunteer details page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -42,6 +42,14 @@ async function run() {
     app.get('/volunteer-need', async(req, res)=>{
         const cursor = volunteerCollection.find().sort({deadline: 1}).limit(6);
         const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    // volunteer details
+    app.get('/volunteer-details/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await volunteerCollection.findOne(query)
         res.send(result);
     })
 
